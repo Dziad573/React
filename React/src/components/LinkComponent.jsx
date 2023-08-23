9026e456bc4703b6ac6c569d97291afabeebb3a9
Dziad573 <krystiansmagacz05@gmail.com>
2023-08-23 21:25:02 +0200

    r2

---
+++ React/src/components/LinkComponent.jsx
@@ -0,0 +1,11 @@
+export function LinkComponent() {
+    return (
+        <a 
+            href="" 
+            target="_blank" 
+            rel="noopener noreferrer"
+        >
+            Odwiedź nas
+        </a>
+    );
+}
--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,48 +1,19 @@
-//import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { LinkComponent } from "./components/LinkComponent";
 
-/*const elementInJSX = <a href="">Test 2</a>;
-const currentDate = new Date();
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-console.log(hours, minutes);
-const timer = (
-  <div>
-    {hours}:{minutes}
-  </div>
-);
-
-ReactDOM.createRoot(document.getElementById('root')).render(timer);*/
-
-const numberContent = "99";
-function getRandomNumber(){
-  return Math.round(Math.random()*100);
-}
-const randomNumber = getRandomNumber();
-
-const paragraphElement = (
-  <>
-    <h2>Heading</h2>
-      <a href="" className="link">
-        Strona
-      </a>
-      <p>
-        Lorem ipsum dolor sit amet 
-        consectetur adipisicing elit. 
-        Dolore, dicta itaque ab velit 
-        accusantium non!
-      </p>
-  </>
-)
 const element = (
-  <div>
-    {`Number: ${numberContent}`}, {`Number: ${randomNumber}`},
-    {paragraphElement},
-    {paragraphElement},
-    {paragraphElement},
-    {paragraphElement},
-    {paragraphElement},
-  </div>
+  <>
+    <h1>Nasza strona!</h1>
+    <main>
+      <LinkComponent/>
+      <p>
+        Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Quo at alias 
+        impedit nemo, ea aperiam.
+      </p>
+      <LinkComponent/>
+    </main>
+  </>
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(element);
