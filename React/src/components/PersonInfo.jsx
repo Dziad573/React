3d7bbf0f05de19e38bee85f62d5caf825ae67a73
Dziad573 <krystiansmagacz05@gmail.com>
2023-09-20 21:40:58 +0200

    tel

--- React/src/components/PersonInfo.jsx
+++ React/src/components/PersonInfo.jsx
@@ -1,5 +1,30 @@
 export function PersonInfo({ person }){
 
+    // NAME
+    let nameSection =(
+        <div>
+                <span>
+                    {person.name} {person.lastName}
+                </span>
+            </div>
+    );
+    
+    if(person.name === undefined) {
+        nameSection = null;
+    }
+
+    // MAIL
+    let mailSection =(
+        <div>
+            E-mail: <a href={`mailto:${person.mail}`}>{person.mail}</a>
+        </div>
+    );
+    
+    if(person.mail === undefined) {
+        mailSection = null;
+    }
+
+    // TEL
     let telSection =(
         <div>
                 Tel: <a href={`tel:${person.tel}`}>{person.tel}</a>
@@ -9,34 +34,13 @@ export function PersonInfo({ person }){
     if(person.tel === undefined) {
         telSection = null;
     }
-
-    let nameSection =(
-        <div>
-                <span>
-                    {person.name} {person.lastName}
-                </span>
-            </div>
-    );
-
-    if(person.name === undefined) {
-        nameSection = null;
-    }
-
-    let mailSection =(
-        <div>
-            E-mail: <a href={`mailto:${person.mail}`}>{person.mail}</a>
-        </div>
-    );
-
-    if(person.mail === undefined) {
-        mailSection = null;
-    }
-
+    
     return (
         <>
             {nameSection}
             {mailSection}
             {telSection}
-    </>
+            
+        </>
     )
 }
