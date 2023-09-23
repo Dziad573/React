31f5b0b865c0d6ffcdc40bf60042e58672e65483
Dziad573 <krystiansmagacz05@gmail.com>
2023-09-23 21:12:50 +0200

    new p

---
+++ React-Mod-2/React/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+
+  return (
+    <>
+      <h1>Pitbul. Nowe porządki</h1>
+      <h2>Rok produkcji: 2016</h2>
+      <h2>Fabuła</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam obcaecati ut deserunt praesentium quisquam. Earum, expedita nisi minus error mollitia harum quod?
+        Cum totam labore quod veniam mollitia fugiat facere consequatur tenetur vitae odit corporis maxime, et ipsam soluta iste qui perspiciatis culpa, omnis eligendi nihil.
+      </p>
+      <button>Pokaż spoiler</button>
+      <p>
+        Perspiciatis modi iusto fugiat optio quibusdam at assumenda, repellendus dolorum culpa pariatur amet, itaque iste dolore eos, reprehenderit eaque vel sunt nostrum.
+        Quod, ut recusandae commodi minima, officia rerum cupiditate laudantium unde quae fuga suscipit quis, et facilis porro cum qui quam sequi earum.
+      </p>
+    </>
+  )
+}
+
+export default App
